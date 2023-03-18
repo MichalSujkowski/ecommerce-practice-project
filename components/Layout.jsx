449ef4f95b6d278b06eb948ae9f01a10fdb71520
Michał Sujkowski <michalsujkowski.fd@gmail.com>
2023-03-18 19:37:32 +0100

    add footer banner

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import {Footer, Navbar} from "@/components/index";
+
+const Layout = ({children}) => {
     return (
-        <div>
-            
+        <div className={'layout'}>
+            <Head>
+                <title>MS Store</title>
+            </Head>
+            <header>
+                <Navbar/>
+            </header>
+            <main className={'main-container'}>
+                {children}
+            </main>
+            <footer>
+                <Footer/>
+            </footer>
         </div>
     );
 };
